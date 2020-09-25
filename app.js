3753b3e6b3ef5b95697a515f89b7daa3a8053c50
Sue Nguyen <SueNguyen1214@gmail.com>
2020-09-24 23:31:21 -0700

    add more features to html, css and js files

--- app.js
+++ app.js
@@ -7,52 +7,52 @@ const store = {
     {
       question: 'What is another name for hard flour?',
       answers: [
-        'Bread flour',
-        'King Arthur flour',
-        'Whole wheat flour',
-        'Ciabatta flour'
+        'A: Bread flour',
+        'B: King Arthur flour',
+        'C: Whole wheat flour',
+        'D: Ciabatta flour'
       ],
-      correctAnswer: 'firstChoice'
+      correctAnswer: 'A'
     },
     {
       question: 'Which type of flour has the lowest gluten content?',
       answers: [
-        'Bread flour',
-        'All purpose flour',
-        'Cake flour',
-        'Almond flour'
+        'A: Bread flour',
+        'B: All purpose flour',
+        'C: Cake flour',
+        'D: Almond flour'
       ],
-      correctAnswer: 'fourthChocie'
+      correctAnswer: 'D'
     },
     {
-      question: 'What is the name of this dish? ',
+      question: 'What is the name of this dish?'+'<br>'+ '<br>'+'<img src="images/salmon_coulibiac.jpg" alt"Russian Coulibiac" style=" width:500px; height:200px;">'+'<br>',
       answers: [
-        'Beef Wellington ',
-        'Shepherd pie',
-        'Russian Salmon Coulibiac',
-        'Tourtiere'
+        'A: Beef Wellington ',
+        'B: Shepherd pie',
+        'C: Russian Salmon Coulibiac',
+        'D: Tourtiere'
       ],
-      correctAnswer: 'thirdChoice'
+      correctAnswer: 'C'
     },
     {
       question: 'Which item does not belong in the traditional full English breakfast? ',
       answers: [
-        'Baked beans',
-        'Grilled tomatoes',
-        'Cabbage',
-        'Marmalade'
+        'A:Baked beans',
+        'B:Grilled tomatoes',
+        'C:Cabbage',
+        'D:Marmalade'
       ],
-      correctAnswer: 'fourthChoice'
+      correctAnswer: 'D'
     },
     {
       question: 'When is the national Cookie Day? ',
       answers: [
-        'December 4th',
-        'September 4th',
-        'January 15th',
-        'May 15th'
+        'A:December 4th',
+        'B:September 4th',
+        'C:January 15th',
+        'D:May 15th'
       ],
-      correctAnswer: 'firstChoice'
+      correctAnswer: 'A'
     },
   ],
   quizStarted: false,
@@ -84,63 +84,64 @@ function welcomeMessage(){
   $('.message').append(`
     <div class="welcome">
       <p>This quiz is to test your knowldge of the culinary world</p>
-      <button type="button" id="startBttn"> Start Quiz</button>
+      <button type="button" class="startBttn"> Start Quiz</button>
     </div>`)
   }
 //This function is to display the question templates
 function questionTemplate(){
-  const currentQuestion=store.questions[store.questionNumber+1].question;
+  const currentQuestion=store.questions[store.questionNumber].question;
   const firstChoice=store.questions[store.questionNumber].answers[0];
   const secondChoice=store.questions[store.questionNumber].answers[1];
   const thirdChoice=store.questions[store.questionNumber].answers[2];
   const fourthChoice=store.questions[store.questionNumber].answers[3];
+  const totalQuestion=store.questions.length;
   //fcvconst rightAnswer=store.questions[store.questionNumber].correctAnswer;
   $('.message').html(`
     <div class="questionNumber">
     
-      <h3> Question Number:${store.questionNumber+1}/5</h3>
+      <h3> Question Number:${store.questionNumber+1}/${totalQuestion}</h3>
       <h3 id="currentScore"> Score:${store.score}</h3>
     
     </div>
     <div class="quizQuestion">
       <form class="quizChoice">
         <h2> ${currentQuestion}</h2>
-          <input name="choice" type="radio" value ="firstChoice">
-            <label for ="firstChoice">${firstChoice}</label><br>
-          <input name="choice" type="radio" value ="secondChoice">
-            <label for ="secondChoice">${secondChoice}</label><br>
-          <input name="choice" type="radio" value ="thirdChoice">
-            <label for ="thirdChoice">${thirdChoice}</label><br>
-          <input name="choice" type="radio" value ="fourthChoice">
-            <label for ="fourthChoice">${fourthChoice}</label><br>
+          <input name="choice" type="radio" required value ="A">
+            <label for ="A">${firstChoice}</label><br>
+          <input name="choice" type="radio" value ="B">
+            <label for ="B">${secondChoice}</label><br>
+          <input name="choice" type="radio" value ="C">
+            <label for ="C">${thirdChoice}</label><br>
+          <input name="choice" type="radio" value ="D">
+            <label for ="D">${fourthChoice}</label><br>
+            <button type="submit" class=" submitBttn"> Submit</button>
       </form>
-      <button type="submit" class="submitBttn"> Submit</button>
+      
     </div>`)
   }
 
-//This function is to display the answer for each question
-function questionAnswer(){
 
-}
 //this fucntion is to display the final result template
 function finalResult(){
-
+  $('.message').html(`
+    <p>Your Score is:${store.score}</p>
+  `)
 }
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
-function renderQuiz(){
+function displayWelcomeMessage(){
   if (store.quizStarted===false){
     welcomeMessage()
   }
 }
 /********** EVENT HANDLER FUNCTIONS **********/
 // These functions handle events (submit, click, etc)
-//This function is for when the start the quiz button is clicked
 
+//This function is for when the start the quiz button is clicked
 function startQuiz(){
   //when the users click on the start Quiz button, take them to the first question
-  $('#startBttn').on('click', ()=>{
+  $('.startBttn').on('click', ()=>{
     $('.welcome').hide();// first hide the welcome message
     questionTemplate();
   })
@@ -151,12 +152,14 @@ function checkAnswer(){
   const rightAnswer=store.questions[store.questionNumber].correctAnswer;
   let messageText="";
   $('.submitBttn').hide();
+  $('.quizChoice').children('input').attr('disabled',true);
+  
   if (chosenAnswer===rightAnswer){
     store.score++;   
-    messageText = "<p>That is correct</p>";
+    messageText = `<p class ="correctMessage">That is correct</p>`;
     $('#currentScore').text('Score: ' + store.score);
   } else {
-      messageText = "<p>That is incorrect</p>";  
+      messageText = `<p class="incorrectMessage">That is incorrect. The correct answer is ${rightAnswer}</p>`;  
     }
   $(`label[for='${chosenAnswer}']`).after(messageText);
   $('.quizQuestion').append(`
@@ -165,23 +168,45 @@ function checkAnswer(){
 }
 //This function is for submit answer for each question
 function submitAnswer(){
-  $('body').on('click','.submitBttn', ()=>{
-   // $('.message').innerHTML= checkAnswer()
-   checkAnswer();     
-  });
+  $('body').on('submit','.quizChoice', (event)=>{
+    event.preventDefault();
+    checkAnswer();  
+   });
 }
+
 //This function is for moving to the next question
 function nextQuestion(){
-  
+  $('body').on('click', '.nextBttn', () =>{
+    if( store.questionNumber<store.questions.length-1){
+      $('.nextBttn').hide();
+      store.questionNumber++;
+      questionTemplate();    
+    }else{
+      $('.message').html(`
+      <div class="finalResult">
+        <p>Your Final Score is:${store.score}/${store.questions.length}</p>
+       <button type="submit" class="restartBttn"> Restart the Quiz</button>
+      </div>
+      `)
+    }
+  });
 }
+
 //This function is for retsarting the quiz
 function restart(){
-
+$('body').on('click', '.restartBttn', ()=>{
+  $('.finalResult').hide();
+  displayWelcomeMessage();
+  store.questionNumber=0;
+  store.score=0;
+  startQuiz();
+})
 }
-function handleQuizApp(){
-  
-  renderQuiz()
-  startQuiz()  
+function renderQuizApp(){  
+  displayWelcomeMessage()
+  startQuiz() 
+  nextQuestion() 
   submitAnswer()
+  restart();
 }
-$(handleQuizApp);
+$(renderQuizApp);
